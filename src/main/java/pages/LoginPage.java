0481fb3cbe8b7e1e4c49e2c28f1eccf63ee27e0c
w3llw3t <hourly_123@mail.ru>
2023-04-12 21:33:20 +0300

    add allure

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,19 +1,10 @@
 package pages;
 
-import io.qameta.allure.Allure;
 import io.qameta.allure.Step;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
-import static pages.AbstractPage.driver;
 
 public class LoginPage {
     public LoginPage(WebDriver driver) {
@@ -35,68 +26,27 @@ public class LoginPage {
     @Step("Нажать на кнопку 'Войти'")
     public void clickOnLogin() {
         login.click();
-        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        // Attach the screenshot to the Allure report
-        try {
-            Allure.addAttachment("Screenshot", new FileInputStream(screenshot));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
     }
     @Step("Нажать на кнопку регистрации")
     public void clickOnReg() {
         regButton.click();
-        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        // Attach the screenshot to the Allure report
-        try {
-            Allure.addAttachment("Screenshot", new FileInputStream(screenshot));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
     }
 
     @Step("Ввести в поле 'Ваша электронная почта' {email}")
     public void setEmail(String ema) {
         email.sendKeys(ema);
-        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        // Attach the screenshot to the Allure report
-        try {
-            Allure.addAttachment("Screenshot", new FileInputStream(screenshot));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
     }
     @Step("Ввести в поле 'Пароль' {password}")
     public void setPassword(String pass) {
         password.sendKeys(pass);
-        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        // Attach the screenshot to the Allure report
-        try {
-            Allure.addAttachment("Screenshot", new FileInputStream(screenshot));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
     }
     @Step("Нажать на кнопку 'Зарегистрироваться'")
     public void finalClickOnReg() {
         finalClick.click();
-        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        // Attach the screenshot to the Allure report
-        try {
-            Allure.addAttachment("Screenshot", new FileInputStream(screenshot));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+
     }
     @Step("Нажать на кнопку Войти")
     public void clickOnLogIn() {
         logIn.click();
-        File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        // Attach the screenshot to the Allure report
-        try {
-            Allure.addAttachment("Screenshot", new FileInputStream(screenshot));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
     }
 }
--- src/test/java/web/UITest.java
+++ src/test/java/web/UITest.java
@@ -1,21 +1,24 @@
 package web;
 import Elements.MainMenu;
 import io.qameta.allure.Step;
+import io.qameta.allure.Allure;
+import java.io.FileInputStream;
+import java.io.IOException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import pages.AbstractPage;
 import pages.BookPage;
 import pages.LoginPage;
-
-import java.io.IOException;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import javax.imageio.ImageIO;
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
-@Listeners({ io.qameta.allure.testng.AllureTestNg.class })
 public class UITest {
     private WebDriver driver;
 
@@ -23,6 +26,22 @@ public class UITest {
     public void setup() throws IOException {
         loadProperties();
         setupDriver();
+        setupTarget();
+    }
+
+    public static void setupTarget() {
+        File directory = new File("target/screenshots");
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+    }
+    public void takeScreenshot(WebDriver driver, String screenshotName) throws IOException {
+        Screenshot screenshot = new AShot().takeScreenshot(driver);
+        String screenshotPath = "target/screenshots/" + screenshotName + ".png";
+        ImageIO.write(screenshot.getImage(), "PNG", new File(screenshotPath));
+        try (FileInputStream fis = new FileInputStream(screenshotPath)) {
+            Allure.addAttachment(screenshotName, fis);
+        }
     }
 
     @Step("Загрузить конфигурацилнные файлы")
@@ -46,43 +65,55 @@ public class UITest {
 
     /** Тест-кейс по поиску книги */
     @Test
-    public void searchBook () {
+    public void searchBook () throws IOException {
         //todo: шаги тест-кейса
         String url = System.getProperty("site.url");
         driver.get(url); //предусловие: открыта главная страница сайта
 
         MainMenu mainMenu = new MainMenu(driver); //1. Ввести в поле "Книга или автор" - Галапагосы
         mainMenu.setInputSearch("Галапагосы");
+        takeScreenshot(driver, "step1search");
 
         BookPage bookPage = new BookPage(driver);
         bookPage.clickOnBook(); //2. Нажать на иконку полученной книги
+        takeScreenshot(driver, "step2search");
         bookPage.clickOnRead(); //3. Нажать на кнопку 'читать отрывок'
+        takeScreenshot(driver, "step3search");
 
     }
     /** Тест-кейс успешной регистрации */
     @Test
-    public void reg() {
+    public void reg() throws IOException {
         String url = System.getProperty("site.url");
         driver.get(url); //предусловие: открыта главная страница сайта
 
         LoginPage loginPage = new LoginPage(driver);
         loginPage.clickOnLogin(); //1. Нажать на кнопку 'войти'
+        takeScreenshot(driver, "step1");
         loginPage.clickOnReg(); //2. Нажать на кнопку 'Зарегистрироваться'
+        takeScreenshot(driver, "step2reg");
         loginPage.setEmail("nicepal223@gmail.com"); //3. Ввести в поле 'Ваша электронная почта' {email}
+        takeScreenshot(driver, "step3reg");
         loginPage.setPassword("Password!23"); //4. Ввести в поле 'Пароль' {password}
+        takeScreenshot(driver, "step4reg");
         loginPage.finalClickOnReg(); //5. Нажать на кнопку 'Зарегистрироваться'
+        takeScreenshot(driver, "step5reg");
     }
     /** Тест-кейс успешной авторизации */
     @Test
-    public void auth() {
+    public void auth() throws IOException {
         String url = System.getProperty("site.url");
         driver.get(url); //предусловие: открыта главная страница сайта
         LoginPage loginPage = new LoginPage(driver);
 
         loginPage.clickOnLogin(); //1. Нажать на кнопку 'войти'
+        takeScreenshot(driver, "step1auth");
         loginPage.setEmail("nicepal223@gmail.com"); //2. Ввести в поле 'Ваша электронная почта' {email}
+        takeScreenshot(driver, "step2auth");
         loginPage.setPassword("Password!23"); //3. Ввести в поле 'Пароль' {password}
-        loginPage.clickOnLogIn(); //2. Нажать на кнопку 'войти'
+        takeScreenshot(driver, "step3auth");
+        loginPage.clickOnLogIn(); //4. Нажать на кнопку 'войти'
+        takeScreenshot(driver, "step4auth");
     }
     @AfterTest
     public void close() {
